93c2d8d229bc730fc3028643f6199dc36d16a6ae
Pieruigi <pierluigi.zomparelli@gmail.com>
2021-03-23 14:03:01 +0100

    gates completed and speeder enhanced

--- Assets/Scripts/BlackHoleGate.cs
+++ Assets/Scripts/BlackHoleGate.cs
@@ -42,11 +42,9 @@ namespace Zom.Pie
         bool closed = false;
         float time = 0;
 
-        float scaleDefault = 0;
+        float scaleDefault = 1;
+        float animTime = 0.5f;
 
-        float zOpenPosition = -1.013f;
-        float zClosedPosition = 0.1f;
-        
         float warningTime = 3f;
 
         Collider coll;
@@ -54,9 +52,9 @@ namespace Zom.Pie
 
         private void Awake()
         {
-            Init();
+            //scaleDefault = gate.transform.localScale.x;
 
-            scaleDefault = gate.transform.localScale.x;
+            Init();
 
             // Get collider
             coll = GetComponent<Collider>();
@@ -75,23 +73,21 @@ namespace Zom.Pie
         void Update()
         {
 #if UNITY_EDITOR
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                if (busy)
-                    return;
-                //Open();
-                if (closed)
-                {
-                    Open(false);
-                    //closed = false;
-                }
-                else
-                {
-                    Close(false);
-                    //closed = true;
-                }
+            //if (Input.GetKeyDown(KeyCode.A))
+            //{
+             
+            //    if (closed)
+            //    {
+            //        Open(false);
+            //        closed = false;
+            //    }
+            //    else
+            //    {
+            //        Close(false);
+            //        closed = true;
+            //    }
 
-            }
+            //}
             //return;
 #endif
 
@@ -153,34 +149,17 @@ namespace Zom.Pie
 
             if (!forced)
             {
-                busy = true;
-                //Sequence seq = DOTween.Sequence();
-                //seq.OnComplete(HandleOnOpened);
-
-                //seq.Append(leftGate.transform.DOShakeRotation(warningTime, Vector3.up * 15f, 10, 90, false));
-                //seq.Join(rightGate.transform.DOShakeRotation(warningTime, Vector3.up * 15f, 10, 90, false));
-                //seq.Append(leftGate.transform.DOLocalRotate(Vector3.zero, 0.2f, RotateMode.Fast));
-                //seq.Join(rightGate.transform.DOLocalRotate(Vector3.zero, 0.2f, RotateMode.Fast));
-                //seq.Append(leftGate.transform.DOLocalMoveZ(zOpenPosition, 0.2f, false).SetEase(Ease.OutElastic));
-                //seq.Join(rightGate.transform.DOLocalMoveZ(zOpenPosition, 0.2f, false).SetEase(Ease.OutElastic));
-
                 // Open
                 coll.enabled = false;
                 closed = false;
-                gate.transform.DOScale(0, 0.25f); 
+                gate.transform.DOScale(0, animTime).SetEase(Ease.OutBounce); 
             }
             else
             {
                 coll.enabled = false;
                 closed = false;
-                Vector3 pos = leftGate.transform.localPosition;
-                pos.z = zOpenPosition;
-                leftGate.transform.localPosition = pos;
-                pos = rightGate.transform.localPosition;
-                pos.z = zOpenPosition;
-                rightGate.transform.localPosition = pos;
-                leftGate.transform.localEulerAngles = Vector3.zero;
-                rightGate.transform.localEulerAngles = Vector3.zero;
+                gate.transform.localScale = Vector3.zero;
+                
             }
             
             
@@ -191,44 +170,23 @@ namespace Zom.Pie
         {
             if (!forced)
             {
-                //busy = true;
-                //Sequence seq = DOTween.Sequence();
-                //seq.OnComplete(HandleOnClosed);
-
-                //seq.Append(leftGate.transform.DOShakePosition(warningTime, Vector3.forward * 0.2f, 10, 90, false, false));
-                //seq.Join(rightGate.transform.DOShakePosition(warningTime, Vector3.forward * 0.2f, 10, 90, false, false));
-                //seq.Append(leftGate.transform.DOLocalMoveZ(zClosedPosition, 0.2f, false));
-                //seq.Join(rightGate.transform.DOLocalMoveZ(zClosedPosition, 0.2f, false));
-                //seq.Append(leftGate.transform.DOLocalRotate(Vector3.up * 90.0f, 0.2f, RotateMode.Fast).SetEase(Ease.OutElastic));
-                //seq.Join(rightGate.transform.DOLocalRotate(Vector3.up * -90.0f, 0.2f, RotateMode.Fast).SetEase(Ease.OutElastic));
-
                 coll.enabled = true;
                 closed = true;
-                gate.transform.DOScale(scaleDefault, 0.25f);
+                gate.transform.DOScale(scaleDefault, animTime).SetEase(Ease.OutBounce);
             }
             else
             {
                 coll.enabled = true;
                 closed = true;
-                Vector3 pos = leftGate.transform.localPosition;
-                pos.z = zClosedPosition;
-                leftGate.transform.localPosition = pos;
-                pos = rightGate.transform.localPosition;
-                pos.z = zClosedPosition;
-                rightGate.transform.localPosition = pos;
-                leftGate.transform.localEulerAngles = Vector3.up * 90f;
-                rightGate.transform.localEulerAngles = Vector3.up * -90f;
+                gate.transform.localScale = Vector3.one * scaleDefault;
+               
             }
 
         }
 
         void CheckState(bool forced)
         {
-            // The gate is already opening or closing
-            if (busy)
-                return;
 
-            Debug.Log("CheckingState - closed:" + closed);
             if (closed)
             {
                 if (time >= closeLengthScaled)
@@ -255,19 +213,7 @@ namespace Zom.Pie
             CheckState(true);
         }
 
-        void HandleOnClosed()
-        {
-            coll.enabled = true;
-            closed = true;
-            busy = false;
-        }
 
-        void HandleOnOpened()
-        {
-            coll.enabled = false;
-            closed = false;
-            busy = false;
-        }
     }
 
 }
