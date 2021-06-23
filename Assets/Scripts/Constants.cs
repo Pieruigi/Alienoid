ce5c0e63a189460f978a481eebd7a02613e04932
Pieruigi <pierluigi.zomparelli@gmail.com>
2021-06-23 16:03:58 +0200

    wip

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -26,7 +26,7 @@ namespace Zom.Pie
         public static readonly int TopPlayers = 20;
 
         // Level speed info
-        public static readonly float DefaultTimeScale = 1.5f;//2.5f;
+        public static readonly float DefaultTimeScale = 2.0f;//2.5f;
         public static readonly float LevelSpeedStep = 0.2f;
         //public static readonly int MaxLevelSpeed = 1;
 
